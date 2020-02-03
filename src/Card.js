f3e4b3dd33a69bca2f6cd113313cd414fef2a7a7
Luke Steiner <luke.steiner@bigcommerce.com>
2020-02-03 15:55:08 -0600

    first commit

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.css';
+
+function Card() {
+  return (
+ 	  <div class="Card">
+            <button type="button">delete</button>
+            <h3>props.title</h3>
+            <p>props.content</p>
+          </div>
+ );
+}
+
+export default Card;
---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './List.css';
+
+function List() {
+    return(
+        <section class="List">
+        <header class="List-header">
+          <h2>First list</h2>
+        </header>
+        <div class="List-cards">
+    )
+}
+
+export default List; 
